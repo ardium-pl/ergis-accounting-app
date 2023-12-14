2db4485cab8cd4917934c09d1d8c3e6f828d2981
joker876 <joker876x@gmail.com>
2023-12-14 20:39:51 +0100

    implement section 1 and 2

--- client/src/app/pages/main/main.page.ts
+++ client/src/app/pages/main/main.page.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { FileDisplayComponent, FileDropZoneComponent, SectionComponent } from '@components';
+import { FileStorageService } from '@services';
 
 @Component({
     selector: 'app-main',
@@ -14,4 +15,21 @@ import { FileDisplayComponent, FileDropZoneComponent, SectionComponent } from '@
 })
 export class MainPage {
 
+    constructor(public fileStorage: FileStorageService) {}
+
+    onFileUpload(file: File): void {
+        if (file.size > 10 * 1024 * 1024) {
+            alert('Plik musi być mniejszy niż 10 MB');
+            return;
+        }
+        if (!file.name.toLowerCase().endsWith('.prn')) {
+            alert('Plik musi być typu .prn');
+            return;
+        }
+        this.fileStorage.setFile(file);
+    }
+
+    onNegativeValuesInput(v: string): void {
+        
+    }
 }
