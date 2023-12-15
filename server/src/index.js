28fd8c7ee9a7c5465ee3a1443ef00bcbdef0d8cb
WolnyMikolaj <136742392+WolnyMikolaj@users.noreply.github.com>
2023-12-15 19:38:47 +0100

    Server setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,8 @@
-console.log('test');
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`App listening on port: ${port}`);
+});
