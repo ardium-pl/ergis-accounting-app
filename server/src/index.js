d188f15a69d656e8908c88ab8cf3a4707f5d997e
WolnyMikolaj <136742392+WolnyMikolaj@users.noreply.github.com>
2023-12-17 00:56:10 +0100

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,20 @@
-console.log('test');
+const express = require("express");
+const cors = require("cors");
+const faktoringRoute = require("./faktoringRoute");
+const mfgDataRoute = require("./mfgDataRoute");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+// Integrating the routes from the modules
+app.use(faktoringRoute);
+app.use(mfgDataRoute);
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
+
+module.exports = app;
