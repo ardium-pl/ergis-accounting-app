9c5673d936d9545a2e764c2c689052956bbbd298
joker876 <joker876x@gmail.com>
2023-12-16 22:44:03 +0100

    create button component

--- client/src/app/components/index.ts
+++ client/src/app/components/index.ts
@@ -1,3 +1,4 @@
-export * from './section/section.component';
-export * from './file-drop-zone/file-drop-zone.component';
+export * from './button/button.component';
 export * from './file-display/file-display.component';
+export * from './file-drop-zone/file-drop-zone.component';
+export * from './section/section.component';
