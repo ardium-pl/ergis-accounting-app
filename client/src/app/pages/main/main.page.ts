dbeedcece1833829f09ceee1b19d23245f247410
joker876 <joker876x@gmail.com>
2023-12-28 19:24:19 +0100

    Create all sections and merging algorithm (#11)
    * format the text before putting it into the field
    
    * create button component
    
    * improve button and create section 4
    
    * add error box
    
    * create gpt service
    
    * create merger service and data table
    
    * add table styling and temporarily remove http service
    
    * finalize the merger service
    
    * implement "copy unused negatives" section
    
    * create icon button
    
    * section 6 adjustments
    
    * fix error box

--- client/src/app/pages/main/main.page.ts
+++ client/src/app/pages/main/main.page.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
-import { FileDisplayComponent, FileDropZoneComponent, SectionComponent } from '@components';
-import { FileStorageService } from '@services';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, computed, signal, effect, ViewChild, ElementRef } from '@angular/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { ButtonComponent, ErrorBoxComponent, FileDisplayComponent, FileDropZoneComponent, FinalTableComponent, IconButtonComponent, SectionComponent } from '@components';
+import { FileStorageService, FinalMergerObject, GptService, MergerService } from '@services';
+import { ErrorBoxType } from 'src/app/components/error-box/error-box.types';
 
 @Component({
     selector: 'app-main',
@@ -9,13 +12,23 @@ import { FileStorageService } from '@services';
         SectionComponent,
         FileDropZoneComponent,
         FileDisplayComponent,
+        MatProgressSpinnerModule,
+        ButtonComponent,
+        ErrorBoxComponent,
+        HttpClientModule,
+        FinalTableComponent,
+        IconButtonComponent,
     ],
     templateUrl: './main.page.html',
     styleUrl: './main.page.scss'
 })
 export class MainPage {
 
-    constructor(public fileStorage: FileStorageService) {}
+    constructor(
+        public fileStorage: FileStorageService,
+        public gptService: GptService,
+        private mergerService: MergerService,
+    ) { }
 
     onFileUpload(file: File): void {
         if (file.size > 10 * 1024 * 1024) {
@@ -29,7 +42,68 @@ export class MainPage {
         this.fileStorage.setFile(file);
     }
 
-    onNegativeValuesInput(v: string): void {
-        
+    readonly formattedFile = computed(() => {
+        const text = this.fileStorage.fileContent();
+        const startPattern = "---------------------------- -------- -------------- --------";
+        const startIndex = text.indexOf(startPattern) + startPattern.length;
+        const endIndex = text.lastIndexOf("faktoring") + "faktoring".length;
+
+
+        let formatted = text;
+        if (startIndex > startPattern.length - 1 && endIndex > "faktoring".length - 1 && endIndex > startIndex) {
+            formatted = formatted.substring(startIndex, endIndex);
+        }
+        formatted = formatted.trim().split('\n').map(v => v.trim()).join('\n');
+        return formatted;
+    });
+
+    results = true;
+    readonly areResultsLoading = signal(false);
+    readonly wasNegativesTouched = signal(false);
+
+    readonly errorBoxState = computed<ErrorBoxType>(() => {
+        if (!this.wasNegativesTouched()) return ErrorBoxType.Info;
+        if (!this.isWrongFormat()) {
+            return ErrorBoxType.Error;
+        }
+        if (this.mergerService.negativesData().length == 0) {
+            ErrorBoxType.Error
+        }
+        return ErrorBoxType.Success;
+    });
+    readonly isWrongFormat = this.mergerService.isNegativeDataValid;
+
+    onNegativeValuesBlur(v: string): void {
+        this.wasNegativesTouched.set(true);
+        this.mergerService.setNegativesData(v);
+    }
+
+    tableData = signal<FinalMergerObject[] | null>([
+        { referencjaKG: 'JL1', currencyCorrection: 200 },
+        { referencjaKG: 'JL2', currencyCorrection: -100 },
+        { referencjaKG: 'JL3', currencyCorrection: 0 },
+        { referencjaKG: 'JL4', currencyCorrection: 50 },
+    ]);
+    readonly unusedNegatives = signal("\nWszystkie pozycje zostały wykorzystane!");
+    readonly hasAnyUnusedNegatives = signal(false);
+    onGenerateButtonClick(): void {
+        const processedData = this.mergerService.processedData();
+        if (!processedData) {
+            this.tableData.set(null);
+            return;
+        }
+        const [data, negatives] = processedData;
+        this.tableData.set(data);
+        if (negatives.length > 0) {
+            this.hasAnyUnusedNegatives.set(false);
+            this.unusedNegatives.set('\nWszystkie pozycje zostały wykorzystane!');
+        } else {
+            this.hasAnyUnusedNegatives.set(true);
+            this.unusedNegatives.set(JSON.stringify(negatives));
+        }
+    }
+
+    copyUnusedNegatives(): void {
+        navigator.clipboard.writeText(this.unusedNegatives());
     }
 }
