2716d59c159ed6925b48ae2e7d655618f115f2d1
joker876 <joker876x@gmail.com>
2023-12-28 18:47:00 +0100

    create icon button

--- client/src/app/components/index.ts
+++ client/src/app/components/index.ts
@@ -1,4 +1,5 @@
 export * from './button/button.component';
+export * from './icon-button/icon-button.component';
 export * from './error-box/error-box.component';
 export * from './file-display/file-display.component';
 export * from './file-drop-zone/file-drop-zone.component';
