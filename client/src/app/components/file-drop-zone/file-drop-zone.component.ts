64a72d17a850df0da21276a094eeed6a7abd8590
joker876 <joker876x@gmail.com>
2024-01-12 22:12:10 +0100

    start creating polymerscan page

--- client/src/app/components/file-drop-zone/file-drop-zone.component.ts
+++ client/src/app/components/file-drop-zone/file-drop-zone.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, Input } from '@angular/core';
+import { coerceArrayProperty } from '@ardium-ui/devkit';
 
 @Component({
     selector: 'app-file-drop-zone',
@@ -13,6 +14,11 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class FileDropZoneComponent {
 
     @Output('upload') uploadEvent = new EventEmitter<File>();
+
+    private _accept: string = '*';
+    @Input()
+    get accept(): string { return this._accept; }
+    set accept(v: any) { this._accept = coerceArrayProperty(v).join(','); }
 
     readFile(event: Event) {
         const file = (event.target as HTMLInputElement).files?.[0];
--- client/src/app/pages/polymerscan/polymerscan.page.ts
+++ client/src/app/pages/polymerscan/polymerscan.page.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FileDisplayComponent, FileDropZoneComponent, SectionComponent } from '@components';
+import { FileStorageService } from '@services';
 
 @Component({
   selector: '_polymerscan-page',
   standalone: true,
-  imports: [],
+  imports: [SectionComponent, FileDropZoneComponent, FileDisplayComponent],
   templateUrl: './polymerscan.page.html',
   styleUrl: './polymerscan.page.scss',
 })
 export class PolymerscanPage {
-    
+  constructor(public fileStorage: FileStorageService) {}
+
+  onFileUpload(file: File): void {
+    if (file.size > 10 * 1024 * 1024) {
+      alert('Plik musi być mniejszy niż 10 MB');
+      return;
+    }
+    if (!file.name.toLowerCase().endsWith('.pdf')) {
+      alert('Plik musi być typu .pdf');
+      return;
+    }
+    this.fileStorage.setFile(file);
+  }
 }
