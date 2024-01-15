e168427c1866b44df3fcb95ade0a154cae70ae1a
joker876 <joker876x@gmail.com>
2024-01-16 00:46:05 +0100

    some frontend fixes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { MainPage, FaktoringPage } from '@pages';
+import { PolymerscanPage } from './pages/polymerscan/polymerscan.page';
 
 export const routes: Routes = [
     { path: "", component: MainPage },
     { path: "faktoring", component: FaktoringPage },
-    { path: "polymerscan", component: FaktoringPage },
+    { path: "polymerscan", component: PolymerscanPage },
     { path: "**", redirectTo: "" }
 ];
