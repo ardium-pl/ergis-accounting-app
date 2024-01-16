0a76109857580fe32390aa28f59b6c881bb9d790
joker876 <joker876x@gmail.com>
2024-01-16 18:45:41 +0100

    manually merge mfgData

--- server/src/api/mfgData.js
+++ server/src/api/mfgData.js
@@ -3,46 +3,46 @@ import axios from "axios";
 import path from "path";
 
 export default async (req, res) => {
-    const mfgRawData = req.body.mfgRawData;
+    const { mfgRawData } = req.body;
 
     if (!mfgRawData) {
         return res.status(400).send("Missing mfgRawData");
     }
 
     try {
-        const filePath = path.join(__dirname, '../system/faktoring_system_message.txt');
-        const systemMessage = fs.readFileSync(filePath, 'utf8');
+        const smallerStrings = mfgRawData.split('\n');
+        
+        var formatedArray = [];
+        smallerStrings.forEach((line) =>
+            formatedArray.push(convertDataToJSON(line)));
+        
+        var formatedArray = smallerStrings.map(line => convertDataToJSON(line));
 
-        const apiKey = process.env.OPEN_AI_KEY;
-        const url = 'https://api.openai.com/v1/chat/completions';
-
-        const response = await axios.post(url, {
-            model: 'gpt-4-1106-preview',
-            messages: [
-                { role: 'system', content: systemMessage },
-                { role: 'user', content: mfgRawData }
-            ],
-            response_format: { type: 'json_object' }
-        }, {
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${apiKey}`
-            },
-            timeout: 180000
-        });
-
-        const assistantResponse = response.data.choices[0].message.content;
-        res.status(201).json({ assistantResponse });
+        res.status(201).json(JSON.stringify(formatedArray));
     } catch (error) {
-        if (error.response) {
-            console.error(error.response.data);
-            res.status(500).send("Error with OpenAI response");
-        } else if (error.request) {
-            console.error(error.request);
-            res.status(500).send("No response from OpenAI");
-        } else {
-            console.error('Error', error.message);
-            res.status(500).send("Internal Server Error");
-        }
+        console.error('Error', error.message);
+        res.status(500).send("Internal Server Error. Contact administrator at mikolaj.wolny@euvic.com");
     }
 };
+
+function convertDataToJSON(dataString) {
+    // Splitting the string into an array of phrases by spaces. Like this:
+    //|JL231004000027|1|264|26403|0000|jza|23/10/03|23/10/04|eur|108,315.58|
+    //|499,237.34|499,237.34|0.00|Nie|JL|JL231004000027|23j20097|faktoring|
+    const parts = dataString.split(/\s+/);
+
+    const referencjaKG = parts[0];
+    const naDzien = parts[6];
+    const kwotaWWalucie = parts[9];
+    const kwotaWZł = parts[10];
+    const korekta = parts[13];
+
+    const jsonObject = {
+        referencjaKG,
+        naDzien,
+        kwotaWWalucie,
+        kwotaWZł,
+        korekta
+    };
+    return jsonObject;
+}
