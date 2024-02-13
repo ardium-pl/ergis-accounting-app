3b26c6f0d425c56a1903f5eee1d9690b96111685
joker876 <joker876x@gmail.com>
2024-02-13 20:36:55 +0100

    try fixing

--- server/src/index.js
+++ server/src/index.js
@@ -11,11 +11,16 @@ app.use(express.json());
 const router = express.Router();
 
 router.get('/', (req, res) => {
+    console.log('Server received a request');
     res.send('The server is working!');
-})
+});
+
+app.use(router);
 
 // app.use(apiRouter);
 // app.use(clientRouter);
+
+console.log('Server is starting up...');
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
