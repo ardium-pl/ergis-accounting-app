0abac9689468bb2dff43566f2378a5090a58b98b
joker876 <joker876x@gmail.com>
2024-02-13 20:07:08 +0100

    try something

--- server/src/index.js
+++ server/src/index.js
@@ -10,8 +10,14 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('The server is working!');
+})
+
 app.use(apiRouter);
-app.use(clientRouter);
+// app.use(clientRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
