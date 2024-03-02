9296a25b2258d0eaf193edc86d4ff45d0a72ae25
joker876 <joker876x@gmail.com>
2024-03-02 22:20:46 +0100

    final changes

--- client/src/app/components/card/card.component.ts
+++ client/src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
-
+    readonly header = input.required<string>();
 }
