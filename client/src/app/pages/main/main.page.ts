a2e425aa6a39b4de8fbc680c42343d153c3dee53
joker876 <joker876x@gmail.com>
2024-03-02 22:28:15 +0100

    Graphics (#43)
    * changes in graphics
    
    * Drawer support data (#42)
    
    * start creating csv service
    
    * create readAsCsv method
    
    * Added editable data table for .prn content
    
    * simple improvements
    
    * cleanup editable table
    
    * start fixing faktoring table css
    
    * improve faktoring table
    
    * improve editable table
    
    * improve editable table headers
    
    * Added data to support daktoring drawer
    
    * Added positive and negative references for lookup
    
    * Added a minus sign "-" to retrieve negatives well
    
    * finish faktoring tooltip
    
    * csv file support
    
    * remove temporary data & cleanup console.logs
    
    * copy over saving leftovers as csv
    
    * cleanup
    
    * improve tooltip
    
    * Major bug fixes
    
    * faktoring service & final-table & editable-table changes
    
    * add konto & subkonto to FinalFaktoringObject
    
    * change naDzień mapping
    
    * finish csv handling in faktoring
    
    * cleanup
    
    * Adding colors ang fixing problems
    
    * removing select from algorythm
    
    * Revert "Merge pull request #39 from ergis-accounting-app/Graphics"
    
    This reverts commit d2a3c7e0d2aaa6a0eaff90b754d895c268a6ebce, reversing
    changes made to e87f20cc745397dea7f9db92248cc955c49519d8.
    
    ---------
    
    Co-authored-by: WolnyMikolaj <kopaniawungla@gmail.com>
    Co-authored-by: WolnyMikolaj <136742392+WolnyMikolaj@users.noreply.github.com>
    Co-authored-by: lolocob7 <147922448+lolocob7@users.noreply.github.com>
    
    * couple fixes
    
    * final changes
    
    ---------
    
    Co-authored-by: Jakub <brodka.jakub@gmail.com>
    Co-authored-by: WolnyMikolaj <kopaniawungla@gmail.com>
    Co-authored-by: WolnyMikolaj <136742392+WolnyMikolaj@users.noreply.github.com>
    Co-authored-by: lolocob7 <147922448+lolocob7@users.noreply.github.com>

--- client/src/app/pages/main/main.page.ts
+++ client/src/app/pages/main/main.page.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CardComponent } from 'src/app/components/card/card.component';
 
 @Component({
   selector: '_main-page',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CardComponent],
   templateUrl: './main.page.html',
   styleUrl: './main.page.scss',
 })
