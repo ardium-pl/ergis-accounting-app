b27edc85c6fdd0bf5f24ce3573da9d789f57bb93
joker876 <joker876x@gmail.com>
2024-06-09 20:05:33 +0200

    create MAPZ parser

--- client/src/app/services/weird-prn-reader/mapz.ts
+++ client/src/app/services/weird-prn-reader/mapz.ts
@@ -1,7 +1,27 @@
+import { _ModelBase } from "./_model-base";
+import { PZItem } from "./pz-item";
 
 
-export class MAPZItem {
-  constructor(lines: string[], headerLines: string[]) {
+export class MAPZItem extends _ModelBase {
+  public readonly dataPod!: Date;
+  public readonly naDzien!: Date;
+  public readonly dataWplywu!: Date;
+
+  public readonly pzItems: PZItem[] = [];
+
+  constructor(contentLines: string[], vatLines: string[]) {
+    super(contentLines, vatLines);
     
+    const line1 = contentLines[1].split(' ');
+
+    this.dataPod = new Date('20' + line1[this._invoiceDateIndex + 1]);
+    this.naDzien = new Date('20' + line1[this._invoiceDateIndex + 2]);
+    this.dataWplywu = new Date('20' + line1[this._invoiceDateIndex + 3]);
+
+    for (const line of contentLines) {
+      if (/PZ .+? \d\d\/\d\d\/\d\d /.test(line)) {
+        this.pzItems.push(new PZItem(line));
+      }
+    }
   }
 }
