89a4e076ce35ec14859bb28bf1bed7057bc29b97
joker876 <joker876x@gmail.com>
2024-07-28 01:33:09 +0200

    fix PZN supplier name sometimes having weird character sequences (#104)
    * Included data modifications for Vat verification
    
    * Fixed CSV type mismatch
    
    * Some progess was made
    
    * quick save
    
    * Added texts and removed and commented unused
    
    * parsed pzn wnpz mapz data
    
    * Update jpk.service.ts
    
    * implemented excell file generation and download
    
    * wstępne formatowanie excella
    
    * added headers to excell
    
    * fixed pzn and rejz
    
    * fixed headers
    
    * fix PZN sometimes having NaNs in wylKosztKG
    
    * added check formula
    
    * implemented transaction number extraction
    
    * implemented all excell formulas
    
    * fix PZN supplier name sometimes having weird character sequences
    
    * Fixed xlsx import issue
    
    * fix to read negative values in PZ items
    
    * implemented csv parser
    
    * fix that 0 is a number not a string
    
    * w trakcie pisania parsera xml
    
    * fixed
    
    * finished xml parser
    
    * fixed and commented
    
    * fixed nip xml formula
    
    * implemented xml formulas (alpha version)
    
    * colors to between rows
    
    * header colors
    
    * border-color
    
    * minor code improvements
    
    * jpk zakupy repair and filters
    
    * fixed errorcheck
    
    * hotfix
    
    * conditional formatting in ErrorCheckSheet
    
    * fix xml formulas
    
    * fixed xml formulas
    
    * conditional formatting to paint 'false' cell in red
    
    ---------
    
    Co-authored-by: WolnyMikolaj <136742392+WolnyMikolaj@users.noreply.github.com>
    Co-authored-by: apizon813 <148215546+apizon813@users.noreply.github.com>
    Co-authored-by: apizon813 <aleksanderpizon@gmail.com>
    Co-authored-by: Jakub <brodka.jakub@gmail.com>

---
+++ client/src/app/services/jpk/jpk.types.ts
@@ -0,0 +1,221 @@
+export type xmlObject = {
+  
+}
+
+export type xmlRecord = {
+  lpzakupu: string;
+  kodkrajunadaniatin: string;
+  nazwadostawcy: string;
+  nrdostawcy: string;
+  dowodzakupu: string;
+  datazakupu: string;
+  k_40: string;
+  k_41: string;
+  k_42: string;
+  k_43: string;
+  k_44: string;
+  k_45: string;
+  k_46: string;
+  k_47: string;
+  datawplywu?: string;
+}
+
+export type csvVerificationRecord = {
+  'Data płatności': string;
+  'Data płatności ze skontem': string;
+  Kompensaty: string;
+  Kontrahent: string;
+  Lp: string;
+  NIP: string;
+  'Numer faktury': string;
+  'Numer faktury korygowanej': string;
+  'Numer referencyjny': string;
+  'Numer wewnętrzny': string;
+  'Numer własny': string;
+  Opis: string;
+  'Opis (dekretacja)': string;
+  Przedpłaty: string;
+  Rejestr: string;
+  Skonto: string;
+  'Status płatności': string;
+  'Termin płatności': string;
+  'Typ faktury': string;
+  Waluta: string;
+  'Wartość skonta': string;
+  ZalacznikiTest: string;
+};
+
+export type readyVerifRecord = {
+  'NIP i numer': string;
+} & csvVerificationRecord;
+
+
+
+type VatItem = {
+  netValue: number;
+  vat: string;
+  vatPercent: number;
+  vatValue: number;
+};
+
+type VatSummary = {
+  netValue: number;
+  vatValue: number;
+  grossValue: number;
+};
+
+export type rejzPrnData = {
+  num: number;
+  reference: string;
+  package: string;
+  type: string;
+  vatNumber: string | null;
+  supplier: string;
+  invoice: string;
+  invoiceDate: Date;
+  vatItems: VatItem[];
+  vatSummary: VatSummary;
+};
+
+export type rejzObject = {
+  num: number;
+  reference: string;
+  package: string;
+  type: string;
+  supplier: string;
+  netValue: number;
+  vatCode: string;
+  vatPercent: number;
+  vatValue: number;
+  invoice: string;
+  invoiceDate: Date;
+};
+
+type PZNSubitem = {
+  dokDost: number;
+  num: number;
+  odchKGZZ: number;
+  receiveDate: Date;
+  sendDate: Date;
+  specNum: string;
+  opcjaERS: string;
+  wylKosztKG: number;
+  zewnPodatekZZ: number;
+}
+
+export type pznPrnData = {
+  commission: string;
+  subitems: PZNSubitem[];
+  supplierName: string;
+  supplierNumber: string;
+  // currencyInfo: PZNCurrencyData;
+};
+
+export type pznObject = {
+  commission: string,
+  dokDost: number;
+  num: number;
+  odchKGZZ: number;
+  receiveDate: Date;
+  sendDate: Date;
+  specNum: string;
+  opcjaERS: string,
+  wylKosztKG: number;
+  zewnPodatekZZ: number;
+  supplierName: string;
+  supplierNumber: string;
+}
+
+type PZNCurrencyData = {
+  original: string;
+  target: string;
+  rate: number;
+};
+
+type PZItem = {
+  code: string;
+  deliveryDate: Date;
+  PZAmount: number;
+  PZAmountUnit: string;
+  invoiceAmount: number;
+};
+
+export type wnpzPrnData = {
+  num: number;
+  reference: string;
+  package: string;
+  type: string;
+  vatNumber: string;
+  supplier: string;
+  dataPod: Date;
+  naDzien: Date;
+  dataWplywu: Date;
+  pzItems: PZItem[];
+  vatItems: VatItem[];
+  invoice: string;
+  invoiceDate: Date;
+  vatSummary: VatSummary;
+};
+
+export type wnpzObject = {
+  num: number;
+  reference: string;
+  package: string;
+  type: string;
+  vatNumber: string;
+  supplier: string;
+  dataPod: Date;
+  naDzien: Date;
+  dataWplywu: Date;
+  code: string;
+  deliveryDate: Date;
+  PZAmount: number;
+  // PZAmountUnit: string;
+  invoiceAmount: number;
+  netValue: number;
+  vat: string;
+  vatPercent: number;
+  vatValue: number;
+  invoice: string;
+  invoiceDate: Date;
+  // grossValue: number;
+}
+
+export type mapzPrnData = {
+  dataPod: Date;
+  dataWplywu: Date;
+  invoice: string;
+  invoiceDate: Date;
+  naDzien: Date;
+  num: number;
+  package: string;
+  pzItems: PZItem[];
+  reference: string;
+  supplier: string;
+  type: string;
+  vatItems: VatItem[]
+  vatNumber: string;
+  vatSummary: VatSummary;
+}
+
+export type mapzObject = {
+  dataPod: Date;
+  dataWplywu: Date;
+  invoice: string;
+  invoiceDate: Date;
+  naDzien: Date;
+  num: number;
+  package: string;
+  code: string;
+  deliveryDate: Date;
+  PZAmount: number;
+  invoiceAmount: number;
+  reference: string;
+  supplier: string;
+  type: string;
+  netValue: number;
+  vat: string;
+  vatPercent: number;
+  vatValue: number;
+  vatNumber: string;
+}
