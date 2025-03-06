e51514306eb0f8bf380b77ef33c5f638ce0b4a66
joker876 <joker876x@gmail.com>
2025-03-06 19:06:39 +0100

    improve parseNumber (#113)

--- client/src/app/utils/helpers.ts
+++ client/src/app/utils/helpers.ts
@@ -1,13 +1,20 @@
-
-
 export function parseNumber(str: string | number): number {
-    return typeof str === 'string' ? Number(str.replace(/\s/g, '').replaceAll(',', '')) : str;
+  if (typeof str === 'number') {
+    return str;
+  }
+  const separator = str.match(/([.,])\d+$/)?.[1];
+  return Number(
+    str
+      .replace(/\s/g, '')
+      .replace(new RegExp(`[^\\d${separator}\\-]`, 'g'), '')
+      .replace(',', '.')
+  );
 }
 
 export function parseYesNo(v: any): boolean {
-    return v === true || v?.toLowerCase() === "tak";
+  return v === true || v?.toLowerCase() === 'tak';
 }
 
 export function randomBetween(min: number, max: number): number {
-    return Math.floor(Math.random() * (max - min) + 1) + min;
+  return Math.floor(Math.random() * (max - min) + 1) + min;
 }
